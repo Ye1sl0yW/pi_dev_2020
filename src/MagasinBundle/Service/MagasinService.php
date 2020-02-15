1ec18a9240cf284c098ff3c7b77509791b51f888
helleuch <haroun.elleuch@esprit.tn>
2020-02-15 19:04:11 +0100

    Ajout de 2 fonctionalités métier à completer

--- src/MagasinBundle/Service/MagasinService.php
+++ src/MagasinBundle/Service/MagasinService.php
@@ -32,5 +32,28 @@ class MagasinService
         return sizeof($inventaire);
     }
 
+    public function calculateStockByCategory($id, $category)
+    {
+        //TODO: Implémenter les catégories correctement
+        $inventaire = $this->em->getRepository(Produit::class)->findBy(array("id_magasin"=>$id));
+        return sizeof($inventaire);
+    }
+
+    public function value($id)
+    {
+        //TODO Faire le service value product
+        $result = 0;
+        $magasin= $this->em->getRepository(Magasin::class)->find($id);
+        return $result;
+    }
+
+    public function createDefaultMagasin()
+    {
+        $mg = new Magasin();
+        $mg->setTailleStock(0);
+        $mg->setMatriculeFiscal(0);
+        //TODO: Faire la jointure vendeur <-> Magasin
+        return $mg;
+    }
 
 }
